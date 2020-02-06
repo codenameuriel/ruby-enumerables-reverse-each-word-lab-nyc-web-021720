7414123447b1c173f863f723244c7b9946803967
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-02-06 00:03:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,5 @@
-def reverse_each_word(string)
-    array_of_string = string.split(" ") 
-    i = 0 
-    while i < array_of_string.length do
-        array_of_string[i].reverse!
-        i += 1
-    end
-    new_string = array_of_string.join(' ')
-    return new_string
+def the_method(string)
+    array_of_string_words = string.split(" ")
+    reversed = array_of_string_words.collect { |word| word.reverse }
+    reversed.join( " ")
 end
-
