47414b484c5b4ce6eba72b5d8bc3b0b11c3354a8
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-02-06 00:07:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
     array_of_string_words = string.split(" ")
     reversed = array_of_string_words.collect { |word| word.reverse }
-    return reversed.join( " ")
+    return reversed.join(" ")
 end
